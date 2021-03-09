69b578be61b3039c56f7a8a39f589d981b9f811d
Joonyoung Park <joonyoungpark33@gmail.com>
2021-03-09 18:55:04 +0900

    Added the all-in-one script.

---
+++ codeql-repo/main/all.sh
@@ -0,0 +1,26 @@
+# Define environment variable `CODE_QL_HOME`
+if [[ -z "${CODE_QL_HOME}" ]]; then
+  WD=`pwd -P`
+  cd ../../
+  CUR=`pwd -P`
+  export CODE_QL_HOME=$CUR
+  cd $WD
+  export PATH="$CODE_QL_HOME/codeql:$PATH"
+fi
+echo CODE_QL_HOME=$CODE_QL_HOME
+# Clean all (dbscheme, db)
+./clean.sh
+
+# Create DB Schemes
+cd dbscheme
+./run.sh
+cd ..
+
+# Dump a trap file for each language
+./trap.sh
+
+# Create mereged db from trap files.
+./merge.sh
+
+# Run the test query.
+./ql/query.sh
