157904bc4f578e87235d223f92337ebe27ab52a3
DJ <f52985@gmail.com>
2021-03-11 13:43:13 +0900

    Fix jni cpp source file

--- codeql-repo/main/jni/HelloJNI.cpp
+++ codeql-repo/main/jni/HelloJNI.cpp
@@ -1,11 +1,11 @@
 #include "build/generated/sources/headers/java/main/hello_HelloJNI.h"
 #include <stdio.h>
 
-JNIEXPORT void JNICALL Java_HelloJNI_printHello (JNIEnv * env, jobject obj){
+JNIEXPORT void JNICALL Java_hello_HelloJNI_printHello (JNIEnv * env, jobject obj){
    printf("Hello World!\n");
    return;
 }
 
-JNIEXPORT jint JNICALL Java_HelloJNI_add (JNIEnv * env, jobject obj, jint a, jint b){
+JNIEXPORT jint JNICALL Java_hello_HelloJNI_add (JNIEnv * env, jobject obj, jint a, jint b){
   return a + b;
 }
