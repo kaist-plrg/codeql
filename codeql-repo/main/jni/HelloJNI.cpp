431eb3e68ad6a4f895640aff346a6a353ec8285a
DJ <f52985@gmail.com>
2021-03-30 22:41:52 +0900

    Modify example jni project to represent field flow

--- codeql-repo/main/jni/HelloJNI.cpp
+++ codeql-repo/main/jni/HelloJNI.cpp
@@ -9,10 +9,22 @@ JNIEXPORT jint JNICALL Java_hello_HelloJNI_f (JNIEnv * env, jobject obj, jint c_
   // c callback function
   int c_return = c_callback(c_param);
 
-  //java callback function
+  // java callback function
   jclass cls = env->GetObjectClass(obj);
   jmethodID mid = env->GetMethodID(cls, "java_callback", "(I)I");
   jint java_return = env->CallIntMethod(obj, mid, c_return);
+
+  // java field access
+  jfieldID fid = env->GetFieldID(cls, "java_field", "I");
+  jint java_field_read = env->GetIntField(obj, fid);
+  printf("field: %d", java_field_read);
+  env->SetIntField(obj, fid, 100);
   
+  // java static field access
+  jfieldID static_fid = env->GetStaticFieldID(cls, "java_static_field", "I");
+  jint java_static_field_read = env->GetStaticIntField(cls, static_fid);
+  printf("static field: %d", java_static_field_read);
+  env->SetStaticIntField(cls, static_fid, 1000);
+
   return java_return;
 }
--- codeql-repo/main/jni/src/main/java/hello/HelloJNI.java
+++ codeql-repo/main/jni/src/main/java/hello/HelloJNI.java
@@ -12,9 +12,21 @@ public class HelloJNI {
     return java_callback_param;
   }
 
+  public int java_field;
+  public static int java_static_field;
+
   public static void main(String[] args) throws Exception{
     HelloJNI jni = new HelloJNI();
-    int val = jni.f(42);
+   
+    jni.java_field = 99;
+    jni.java_static_field = 999;
+
+    int val = jni.f(9);
     System.out.println(val);
+
+    int java_field_write = jni.java_field;
+    int java_static_field_write = jni.java_static_field;
+    System.out.println(java_field_write);
+    System.out.println(java_static_field_write);
   }
 }
