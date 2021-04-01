48671c8a9363fdf825b41e1f16ccdf53f59bc231
DJ <f52985@gmail.com>
2021-04-01 14:01:23 +0900

    Make trap.sh and merge.sh accept source root directory

--- codeql-repo/main/merge.sh
+++ codeql-repo/main/merge.sh
@@ -1,5 +1,7 @@
+root=${2:-"jni"}
+
 rm -rf db-merged
-codeql database init -l=merged --source-root=jni db-merged
+codeql database init -l=merged --source-root=$root db-merged
 mkdir db-merged/src
 node rewrite-trap.js
 codeql database finalize --dbscheme=dbscheme/my.merged.dbscheme db-merged
--- codeql-repo/main/trap.sh
+++ codeql-repo/main/trap.sh
@@ -1,13 +1,15 @@
+root=${1:-"jni"}
+
 echo '[Creating database for java]'
 rm -rf db-java
 echo '[init]'
-codeql database init -l=java --source-root=jni db-java
+codeql database init -l=java --source-root=$root db-java
 echo '[trace-command]'
-codeql database trace-command --working-dir=jni db-java "$CODE_QL_HOME/codeql/java/tools/autobuild.sh"
+codeql database trace-command --working-dir=$root db-java "$CODE_QL_HOME/codeql/java/tools/autobuild.sh"
 
 echo '[Creating database for cpp]'
 rm -rf db-cpp
 echo '[init]'
-codeql database init -l=cpp --source-root=jni db-cpp
+codeql database init -l=cpp --source-root=$root db-cpp
 echo '[trace-command]'
-codeql database trace-command --working-dir=jni db-cpp make
+codeql database trace-command --working-dir=$root db-cpp make
