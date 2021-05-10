7deb748549d43ce878bb4865f162ba4a95387bae
DJ <f52985@gmail.com>
2021-05-10 20:34:02 +0900

    Use https for mavenCentral

--- codeql-repo/main/F-Droid/gradle-version-patch.sh
+++ codeql-repo/main/F-Droid/gradle-version-patch.sh
@@ -6,7 +6,10 @@ while read line; do
   echo distributionUrl=https\\://services.gradle.org/distributions/gradle-$3-bin.zip \
   > $1/gradle/wrapper/gradle-wrapper.properties
 
-  if [ ! -z $4 ]; then
+  if [ "$4" == CP_LOCAL_PROPERTIES ]; then
     cp ../local.properties .
+  elif [ "$4" == MAVEN_CENTRAL ]; then
+    sed 's/mavenCentral()/maven { url "https:\/\/repo1.maven.org\/maven2\/" }/g' $1/build.gradle > __tmp__
+    mv __tmp__ $1/build.gradle
   fi
 done < gradle-version-patch.txt
