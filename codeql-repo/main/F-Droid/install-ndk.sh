45f25448b0db4bf6df13ae802d393b7127dfc393
DJ <f52985@gmail.com>
2021-05-10 19:51:02 +0900

    Install ndk with respect to OS

--- codeql-repo/main/F-Droid/install-ndk.sh
+++ codeql-repo/main/F-Droid/install-ndk.sh
@@ -1,3 +1,12 @@
+if [[ $OSTYPE == linux-gnu* ]]; then
+  ndk_old=ndk-old-linux.txt
+elif [[ $OSTYPE == darwin* ]]; then
+  ndk_old=ndk-old-darwin.txt
+else
+  echo "OS not supported"
+  exit 1
+fi
+
 # install old ndks by manually downloading from url
 while read line; do
   echo $line
@@ -11,7 +20,7 @@ while read line; do
   wget -O $zip $1
   unzip -n -q $zip -d ndk
   rm $zip
-done < ndk-old.txt
+done < $ndk_old
 
 # install new ndks using sdkmanager
 while read line; do
