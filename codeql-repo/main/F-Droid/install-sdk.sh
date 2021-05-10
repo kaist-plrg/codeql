66f8f644f4bd19b2dba9329958d8ac72be60d01b
DJ <f52985@gmail.com>
2021-05-10 18:42:50 +0900

    Manually hard-code specific sdk version

--- codeql-repo/main/F-Droid/install-sdk.sh
+++ codeql-repo/main/F-Droid/install-sdk.sh
@@ -1,6 +1,10 @@
 pVersions=`grep compileSdkVersion -r extracted --include build.gradle -h | grep -o "[0-9][0-9]" | sort -u`
 btVersions=`grep buildToolsVersion -r extracted --include build.gradle -h | grep -o "[0-9][0-9]\.[0-9]\.[0-9]" | sort -u`
 
+#org.zamedev.gloomydungeons
+pVersions+=" 17"
+btVersions+=" 19.1.0"
+
 cmd="cmdline-tools/bin/sdkmanager --sdk_root=sdk"
 
 for version in $pVersions; do
