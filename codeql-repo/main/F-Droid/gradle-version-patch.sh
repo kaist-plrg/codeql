5842a32881f36cc45dee8f546faee55ea0a66c3f
DJ <f52985@gmail.com>
2021-05-13 21:59:40 +0900

    Add additional gradle version patch

--- codeql-repo/main/F-Droid/gradle-version-patch.sh
+++ codeql-repo/main/F-Droid/gradle-version-patch.sh
@@ -11,5 +11,8 @@ while read line; do
   elif [ "$4" == MAVEN_CENTRAL ]; then
     sed 's/mavenCentral()/maven { url "https:\/\/repo1.maven.org\/maven2\/" }/g' $1/build.gradle > __tmp__
     mv __tmp__ $1/build.gradle
+  elif [ ! -z "$4" ]; then
+    sed 's/3.0.1/3.1.1/g' $1/build.gradle > __tmp__
+    mv __tmp__ $1/build.gradle
   fi
 done < resource/gradle-version-patch.txt
