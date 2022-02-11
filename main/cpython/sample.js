5c5d7e930615a13f992c0898ec7058c085f64bf2
Dongjun Youn <ydongjun@amazon.co.uk>
2022-02-11 14:31:32 +0000

    Remove unnecessary file

--- main/cpython/sample.js
+++
@@ -1,9 +0,0 @@
-let rel = "rel";
-let PREFIX = "python_";
-
-let pattern = RegExp(`(?<!predicate)\\W${rel}\\((?!\\))`, 'g');
-
-let line = "(rel(1,2) or rel(2,3))";
-
-console.log(line.replaceAll(pattern, ` ${PREFIX}${rel}(`));
-console.log(`(?<!predicate)\\W${rel}\\((?!\\))`);
