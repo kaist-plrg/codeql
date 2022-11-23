a3cdf7fb56a51800377811d58d5dbbce4f2c3953
DJ <f52985@gmail.com>
2022-11-23 17:07:11 +0900

    Add polycruise bench

---
+++ benchmark/cpython-polycruise/src/FieldSensitivity-7_leak_PyClang/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('DemoTrace',
+                    define_macros = [('MAJOR_VERSION', '1'), ('MINOR_VERSION', '0')],
+                    #extra_link_args=[]
+                    #extra_compile_args=[]
+                    include_dirs = ['C/include'],
+                    #libraries = ['DemoTrace'],
+                    #library_dirs = ['/usr/lib'],
+                    sources = ['CPython/Demo.c', 'C/source/Trace.c'])
+
+setup (name = 'PyDemo',
+       version = '1.0',
+       description = 'package for python tracing',
+       author = 'Wen xx',
+       author_email = 'xx.wen@xxx.edu',
+       ext_modules = [module1])
+
