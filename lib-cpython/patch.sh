ce89ada212fbcab46b13e0e8e9c20075e2ae004a
DJ <f52985@gmail.com>
2022-12-19 20:55:50 +0900

    More patch

--- lib-cpython/patch.sh
+++ lib-cpython/patch.sh
@@ -1,2 +1,3 @@
 cp patched/DataFlowPrivate.qll python/semmle/python/dataflow/new/internal/DataFlowPrivate.qll
 cp patched/Flow.qll python/semmle/python/Flow.qll
+cp patched/Function.qll python/semmle/python/Function.qll
